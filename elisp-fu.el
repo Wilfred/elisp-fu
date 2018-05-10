3a1d996ace4eb438008bbd3f5a40ca004898e3e7
Wilfred Hughes <me@wilfred.me.uk>
2018-05-10 07:57:40 +0100

    Basic overlay logic

--- elisp-fu.el
+++ elisp-fu.el
@@ -121,14 +121,123 @@
 
       (list expr start-pos (point)))))
 
+(defun eros--make-overlay (l r type &rest props)
+  "Place an overlay between L and R and return it.
+
+TYPE is a symbol put on the overlay's category property.  It is
+used to easily remove all overlays from a region with:
+
+    (remove-overlays start end 'category TYPE)
+
+PROPS is a plist of properties and values to add to the overlay."
+  (let ((o (make-overlay l (or r l) (current-buffer))))
+    (overlay-put o 'category type)
+    (overlay-put o 'eros-temporary t)
+    (while props (overlay-put o (pop props) (pop props)))
+    (push #'eros--delete-overlay (overlay-get o 'modification-hooks))
+    o))
+
+(defun eros--delete-overlay (ov &rest _)
+  "Safely delete overlay OV.
+
+Never throws errors, and can be used in an overlay's
+modification-hooks."
+  (ignore-errors (delete-overlay ov)))
+
+(cl-defun eros--make-result-overlay (value &rest props &key where duration (type 'result)
+                                           (format (concat " " eros-eval-result-prefix "%s "))
+                                           (prepend-face 'eros-result-overlay-face)
+                                           &allow-other-keys)
+  "Place an overlay displaying string VALUE at the end of the line.
+
+VALUE is used as the overlay's after-string property, meaning it
+is displayed at the end of the overlay.  The overlay itself is
+placed from beginning to end of current line.
+
+Return nil if the overlay was not placed or if it might not be
+visible, and return the overlay otherwise.
+
+Return the overlay if it was placed successfully, and nil if it
+failed.
+
+This function takes some optional keyword arguments:
+
+- If WHERE is a number or a marker, apply the overlay over the
+  entire line at that place (defaulting to `point').  If it is a
+  cons cell, the car and cdr determine the start and end of the
+  overlay.
+
+- TYPE is passed to `eros--make-overlay' (defaults to `result')."
+  (declare (indent 1))
+  (while (keywordp (car props))
+    (setq props (cddr props)))
+  (let ((buffer (current-buffer)))
+    (with-current-buffer buffer
+      (save-excursion
+        ;; Make sure the overlay is actually at the end of the sexp.
+        (skip-chars-backward "\r\n[:blank:]")
+        (let* ((beg (if (consp where)
+                        (car where)
+                      (save-excursion
+                        (backward-sexp 1)
+                        (point))))
+               (end (if (consp where)
+                        (cdr where)
+                      (line-end-position)))
+               (display-string value)
+               (o nil))
+          (remove-overlays beg end 'category type)
+          ;; If the display spans multiple lines or is very long, display it at
+          ;; the beginning of the next line.
+          (when (or (string-match "\n." display-string)
+                    (> (string-width display-string)
+                       (- (window-width) (current-column))))
+            (setq display-string (concat " \n" display-string)))
+          ;; Put the cursor property only once we're done manipulating the
+          ;; string, since we want it to be at the first char.
+          (put-text-property 0 1 'cursor 0 display-string)
+          (when (> (string-width display-string) (* 3 (window-width)))
+            (setq display-string
+                  (concat (substring display-string 0 (* 3 (window-width)))
+                          "...\nResult truncated.")))
+          ;; Create the result overlay.
+          (setq o (apply #'eros--make-overlay
+                         beg end type
+                         'after-string display-string
+                         props))
+          (if this-command
+              (add-hook 'pre-command-hook
+                        #'eros--remove-result-overlay
+                        nil 'local)
+            (eros--remove-result-overlay))
+          (let ((win (get-buffer-window buffer)))
+            ;; Left edge is visible.
+            (when (and win
+                       (<= (window-start win) (point))
+                       ;; In 24.3 `<=' is still a binary predicate.
+                       (<= (point) (window-end win))
+                       ;; Right edge is visible. This is a little conservative
+                       ;; if the overlay contains line breaks.
+                       (or (< (+ (current-column) (string-width value))
+                              (window-width win))
+                           (not truncate-lines)))
+              o)))))))
+
 (defun elisp-fu--eval (expr start-pos end-pos)
   "Evaluate EXPR, flashing its position in the buffer."
-  (let* ((result nil))
+  (let* (result formatted-result)
     (condition-case e
         (progn
           (setq result (eval expr lexical-binding))
           (elisp-fu--flash-region 'elisp-fu-success start-pos end-pos)
-          (message "result: %S" result))
+
+          ;; TODO: use conventional Emacs integer formatting
+          ;; TODO: truncate long string
+          (setq formatted-result (pp-to-string result))
+          (eros--make-result-overlay (format " => %s" formatted-result)
+            :where (point)
+            :duration 'command)
+          (message "%s" formatted-result))
       (error
        ;; Flash an error, then propagate the signal.
        (elisp-fu--flash-region 'elisp-fu-error start-pos end-pos)
